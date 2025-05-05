31ca9ae534a93664e58f30c4b6670d24d20b2b45
marjan <marjan@shuplinoski.com>
2025-05-05 16:47:33 +0200

    Adding CORS to allow remote host to send API requests

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,24 @@ import express from 'express';
 import dotenv from 'dotenv';
 import {connectDB} from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
+import cors from 'cors';
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+      'Access-Control-Allow-Methods',
+      'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
+  next();
+});
+
 app.use('/api/products',productRoutes)
 
 app.listen(process.env.BACKEND_PORT, () => {
